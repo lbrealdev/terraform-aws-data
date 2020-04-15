df634ccc41fdcda90ba16496a03c6f2603a7adb7
Administrator <admin@example.com>
2020-04-15 21:48:31 +0200

    Test outputs module s3 (#002)

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,7 +1,8 @@
 output "bucket_name" {
-  value = data.aws_s3_bucket.main.bucket
+  value = values(data.aws_s3_bucket.main)[*].bucket
 }
 
+/*
 output "bucket_domain_name" {
   value = data.aws_s3_bucket.main.bucket_domain_name
 }
@@ -12,4 +13,4 @@ output "website_domain" {
 
 output "website_endpoint" {
   value = data.aws_s3_bucket.main.website_endpoint
-}
+}*/
--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,7 @@ output "this_bucket_name" {
   value = module.data_s3.bucket_name
 }
 
+/*
 output "this_bucket_domain_name" {
   value = module.data_s3.bucket_domain_name
 }
@@ -16,4 +17,4 @@ output "this_bucket_website_domain" {
 
 output "this_bucket_website_endpoint" {
   value = module.data_s3.website_endpoint
-}
+}*/

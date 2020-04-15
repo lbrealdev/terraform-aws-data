b52df764d40377faa323f9194327a678b9d6ecab
Administrator <admin@example.com>
2020-04-15 21:51:17 +0200

    Test outputs module s3 (#003)

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -2,15 +2,17 @@ output "bucket_name" {
   value = values(data.aws_s3_bucket.main)[*].bucket
 }
 
-/*
 output "bucket_domain_name" {
-  value = data.aws_s3_bucket.main.bucket_domain_name
+  //value = data.aws_s3_bucket.main.bucket_domain_name
+  value = values(data.aws_s3_bucket.main)[*].bucket_domain_name
 }
 
 output "website_domain" {
-  value = data.aws_s3_bucket.main.website_domain
+  //value = data.aws_s3_bucket.main.website_domain
+  value = values(data.aws_s3_bucket.main)[*].website_domain
 }
 
 output "website_endpoint" {
-  value = data.aws_s3_bucket.main.website_endpoint
-}*/
+  //value = data.aws_s3_bucket.main.website_endpoint
+  value = values(data.aws_s3_bucket.main)[*].website_endpoint
+}
--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,6 @@ output "this_bucket_name" {
   value = module.data_s3.bucket_name
 }
 
-/*
 output "this_bucket_domain_name" {
   value = module.data_s3.bucket_domain_name
 }
@@ -17,4 +16,4 @@ output "this_bucket_website_domain" {
 
 output "this_bucket_website_endpoint" {
   value = module.data_s3.website_endpoint
-}*/
+}

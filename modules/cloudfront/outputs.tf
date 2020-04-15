8f148ad2d39e871fd461c631d73f8fdc6435de19
Administrator <admin@example.com>
2020-04-15 21:40:10 +0200

    Test outputs module cloudfront (#002)

--- modules/cloudfront/outputs.tf
+++ modules/cloudfront/outputs.tf
@@ -1,11 +1,3 @@
-locals {
-  cloudfront_id = element(concat(data.aws_cloudfront_distribution.main.*.id, [""]), 0)
-}
-
-output "this_cloudfront_id" {
-  value = local.cloudfront_id
-}
-
-output "id" {
+output "cloudfront_id" {
   value = data.aws_cloudfront_distribution.main.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "this_cloudfront_id" {
-  value = module.data_cloudfront.id
+  value = module.data_cloudfront.cloudfront_id
 }

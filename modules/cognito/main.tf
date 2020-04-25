9c2b823b47c2c6b3a3ea4dc93f80245f576765f4
Administrator <admin@example.com>
2020-04-26 01:28:38 +0200

    Test data module (#029)

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -11,6 +11,6 @@ terraform {
 # ---------------------------------------------------------------------------------------------------------------------
 
 data "aws_cognito_user_pools" "main" {
-  count = var.data || var.name != "" ? length(values(var.name)) : 0
+  count = var.data || var.name != "" ? length(var.name) : 0
   name  = var.name
 }
